e527ad4a758175f94224b024df8402a04ed1ffc8
Max Walters <max.walters10@gmail.com>
2020-07-24 01:30:52 -0400

    Doing it to prove to myself I'm not crazy

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,3 @@
-import 'package:clean_framework/clean_framework.dart';
-import 'package:clean_soundboard/soundboard/ui/main_screen.dart';
-import 'package:clean_soundboard/soundboard/ui/sound_screen.dart';
 import 'package:flutter/material.dart';
 import 'sound/ui/main_screen.dart';
 
