6b89015639ee278346721eee1b9bda49c0b1be60
Maxwell Walters <max.walters10@gmail.com>
2020-07-25 14:21:46 -0400

    Initial UI set

--- lib/sound/ui/main_screen.dart
+++ lib/sound/ui/main_screen.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:clean_framework/clean_framework.dart';
 import 'sound_screen.dart';
+import '../model/sound_view_model.dart';
 
 class MainScreen extends Screen {
+  final SoundViewModel svm = new SoundViewModel();
+
   @override
   Widget build(BuildContext context) {
-    return SoundScreen();
+    return SoundScreen(
+      viewModel: svm,
+    );
   }
 }
