f0add92da4ddb297397509fe2fb6d93f16e71d53
Maxwell Walters <max.walters10@gmail.com>
2020-07-25 19:32:16 -0400

    Comment with testing audio

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+// Audio file for testing - https://s3.amazonaws.com/media.sampleapp.hunt/bell.wav
+
 import 'package:clean_soundboard/sound/ui/sound_widget.dart';
 import 'package:flutter/material.dart';
-import 'sound/ui/sound_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
