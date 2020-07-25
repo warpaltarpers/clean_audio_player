95051bbea1f4cab9925bcb1bcc87a463cc249d19
Maxwell Walters <max.walters10@gmail.com>
2020-07-25 12:37:30 -0400

    Build bloc files

--- lib/sound/bloc/sound_bloc.dart
+++ lib/sound/bloc/sound_bloc.dart
@@ -1,23 +1,29 @@
-import 'dart:async';
-
 import 'package:clean_framework/clean_framework.dart';
-import 'package:audioplayer/audioplayer.dart';
+import '../api/sound_api.dart';
+import 'sound_usecase.dart';
+import 'package:clean_framework_example/payment/model/payment_view_model.dart'; //TODO: Build file
 
 class SoundBloc extends Bloc {
-  final soundPipe = EventPipe();
-  final AudioPlayer audioPlugin = AudioPlayer();
+  SoundUseCase _soundUseCase;
   final url = 'https://s3.amazonaws.com/media.sampleapp.hunt/bell.wav';
-  
+
+  final soundViewModelPipe = Pipe<SoundViewModel>();
+  final soundUrlPipe = Pipe<String>();
+
   @override
   void dispose() {
-    soundPipe.dispose();
+    soundViewModelPipe.dispose();
+    soundUrlPipe.dispose();
   }
-  
-  SoundBloc() {
-    soundPipe.listen(playHandler);
+
+  SoundBloc({SoundService SoundService}) {
+    _soundUseCase = SoundUseCase((viewModel) => soundViewModelPipe.send(viewModel));
+    
+    soundViewModelPipe.onListen(() => _soundUseCase.create());
+    soundUrlPipe.receive.listen(soundUrlInputHandler);
   }
-  
-  void playHandler() {
-    audioPlugin.play(url);
+
+  void amountInputHandler(String soundUrl) {
+    _soundUseCase.playSound(soundUrl);
   }
 }
