7ce6cf3fe909ac1572ada5d6d258a8472c2d6723
Max Walters <max.walters10@gmail.com>
2020-08-28 16:29:14 -0400

    Removing group

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,19 +4,16 @@ import 'package:flutter/material.dart';
 import 'package:clean_soundboard/sound/ui/sound_widget.dart';
 
 void main() {
-  group('Widget Tests:', () {
+  final testWidget = MaterialApp(
+    home: SoundWidget(),
+  );
+
+  testWidgets('Screen builds', (WidgetTester tester) async {
+    await tester.pumpWidget(testWidget);
+    await tester.pump(Duration(milliseconds: 200));
     
-    final testWidget = MaterialApp(
-      home: SoundWidget(),
-    );
-  
-    testWidgets('Screen builds', (WidgetTester tester) async {
-      await tester.pumpWidget(testWidget);
-      await tester.pump(Duration(milliseconds: 200));
-      
-      expect(find.byType(AppBar), findsOneWidget);
-      expect(find.byType(TextFieldListTile), findsOneWidget);
-      expect(find.byIcon(Icons.play_arrow), findsOneWidget);
-    });
+    expect(find.byType(AppBar), findsOneWidget);
+    expect(find.byType(TextFieldListTile), findsOneWidget);
+    expect(find.byIcon(Icons.play_arrow), findsOneWidget);
   });
 }
